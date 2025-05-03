ca45cf74220fea840e061bcf52976903d3fd4e3c
trustgodm <137123568+trustgodm@users.noreply.github.com>
2025-05-04 00:25:34 +0200

    Added CI/CD for tests

---
+++ src/tests/layouts/AuthLayout.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import AuthLayout from "../../layouts/AuthLayout";
+import React from "react";
+import { MemoryRouter } from "react-router-dom";  
+test("renders AuthLayout component", () => {
+  render(
+    <ThemeProvider>
+      <AuthLayout />
+    </ThemeProvider>
+  );
+
+  expect(screen.getByText(/Ireserve. All rights reserved./i)).toBeInTheDocument();
+});
