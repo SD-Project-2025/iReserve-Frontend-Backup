f0ce416357ff485bbbf2166d3c3d7b6268957fbc
Lanet <101481593+2626245@users.noreply.github.com>
2025-05-18 23:27:02 +0200

    included the syntax handling

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,7 +1,5 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    
     '@babel/preset-env',
     '@babel/preset-typescript',
     '@babel/preset-react',
@@ -11,16 +9,3 @@ module.exports = {
     '@babel/plugin-syntax-import-meta',
   ],
 };
-
-
-// module.exports = {
-//   presets: [
-//     '@babel/preset-env',
-//     '@babel/preset-typescript',
-//     '@babel/preset-react',
-//   ],
-//   plugins: [
-    
-//     '@babel/plugin-syntax-import-meta',
-//   ],
-// };
