21c0ba4f6ae99702a7af741eadb96ee29726d6c5
Lanet <101481593+2626245@users.noreply.github.com>
2025-05-18 23:55:05 +0200

    Conflicts resolved

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,5 @@
 module.exports = {
   presets: [
-<<<<<<< HEAD
     '@babel/preset-env',
     '@babel/preset-typescript',
     '@babel/preset-react',
@@ -10,10 +9,4 @@ module.exports = {
     '@babel/plugin-syntax-import-meta',
   ],
 };
-=======
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript"
-  ]
-}
->>>>>>> 9967a30126cc03c15e5c165b8379f52c439f7c0f
+
